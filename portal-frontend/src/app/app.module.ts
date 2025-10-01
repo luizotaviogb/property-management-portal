d5a33c83e7b01c71245ec289ffd7abaa4de01ddf
luizotaviogoebelbrey <luizotaviogb@gmail.com>
2025-10-01 01:38:54 +0100

    add angular material

--- portal-frontend/src/app/app.module.ts
+++ portal-frontend/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { MainComponent } from './components/main/main.component';
 
@@ -10,8 +14,12 @@ import { MainComponent } from './components/main/main.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    MainComponent
+    MainComponent,
+    MatTabsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [
     provideHttpClient(withInterceptorsFromDi())
